90b2468e10b345420210188f92f4005fc59c7da1
nabilputrah <61397812+nabilputrah@users.noreply.github.com>
2021-03-22 11:16:38 +0700

    update 1 (finish)

--- W2/Tugas2W2/src/tugas2w2/ParseInts.java
+++ W2/Tugas2W2/src/tugas2w2/ParseInts.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugas2w2;
 
 /**
@@ -23,10 +18,12 @@ public class ParseInts{
         System.out.println("Enter a line of text");  
         Scanner scanLine = new Scanner(scan.nextLine());  
         while (scanLine.hasNext()){
-            val = Integer.parseInt(scanLine.next());  
-            sum += val;  
+            try {
+                val = Integer.parseInt(scanLine.next());  
+                sum += val;
+            }catch(NumberFormatException e){
+            }
         }
         System.out.println("The sum of the integers on this line is " + sum);   
     }  
-} 
-
+}
