6bc1723a25a65f2e0375eea68149e6a41b846462
nabilputrah <61397812+nabilputrah@users.noreply.github.com>
2021-03-26 09:12:32 +0700

    Update Benerin baris

--- W2/Tugas8W2/src/tugas8w2/PrimePanel.java
+++ W2/Tugas8W2/src/tugas8w2/PrimePanel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugas8w2;
 
 /**
@@ -37,7 +32,7 @@ public class PrimePanel extends JPanel {
         computeButton = new JButton ("Click to see all primes up to your number!");   
         primeList = new JTextArea (10, 30);  
         computeButton.addActionListener(new ButtonListener());
-        
+        //Scroll Panel
         scrollPanel = new JScrollPane(primeList);
         scrollPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         // Add the components to the panel 
